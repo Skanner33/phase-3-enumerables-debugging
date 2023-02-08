2d423f6fd052caf7e5298edd5ef466cd0a0c9d67
Skanner33 <mathewmageto@gmail.com>
2023-02-08 11:11:45 +0300

    code-along

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,1 +1,17 @@
 # Write your code here
+require "pry"
+
+def reverse_each_word(sentence)
+  words = sentence.split
+
+  reversed_words = words.map do |word|
+    word.reverse
+  end
+
+  reversed_words.join(" ")
+end
+
+binding.pry
+0
+
+puts reverse_each_word("Hello there, and how are you?")
